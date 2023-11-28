48fe76fcb5ab9b2c8a3754abfb6e8d369936a1bc
Giorgos Papasteriadis <giorgospapasteriadis@gmail.com>
2023-11-29 01:05:09 +0200

    Added dark/light mode

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using PersonalWebSite;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorageAsSingleton();
 
 await builder.Build().RunAsync();
