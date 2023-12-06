7827c87fd6c5462c0393f3bdb54b0309ecf426e2
Giorgos Papasteriadis <giorgospapasteriadis@gmail.com>
2023-12-06 19:52:21 +0200

    Added localization, UI changes and Theme reconstruction

--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using PersonalWebSite;
+using MudBlazor;
+using PersonalWebSite.Localize;
+using System.Globalization;
+using Microsoft.JSInterop;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,5 +15,26 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 builder.Services.AddBlazoredLocalStorageAsSingleton();
+builder.Services.AddTransient<MudLocalizer, Resource>();
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+
+CultureInfo culture;
+var js = host.Services.GetRequiredService<IJSRuntime>();
+var result = await js.InvokeAsync<string>("blazorCulture.get");
+
+if (result != null)
+{
+    culture = new CultureInfo(result);
+}
+else
+{
+    culture = new CultureInfo("en-US");
+    await js.InvokeVoidAsync("blazorCulture.set", "en-US");
+}
+
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
+    
+await host.RunAsync();
