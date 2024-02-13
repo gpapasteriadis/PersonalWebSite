c28580caee8334a453835604d304232771aa9714
Giorgos Papasteriadis <giorgospapasteriadis@gmail.com>
2024-02-13 15:34:43 +0200

    Moved shared functionality into stateContainer. Implemented some fixes to AppBar's animation when scrolling.

--- SharedState/StateContainer.cs
+++ SharedState/StateContainer.cs
@@ -1,12 +1,32 @@
-﻿namespace PersonalWebSite.SharedState
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace PersonalWebSite.SharedState
 {
-    public class StateContainer
+    public class StateContainer(IJSRuntime jsRuntime, NavigationManager navigationManager)
     {
+        private readonly IJSRuntime _jsRuntime = jsRuntime;
+        private readonly NavigationManager _navigationManager = navigationManager;
+
         public bool DarkModeIsOn { get; set; }
         public bool SideBarIsOpen { get; set; } = false;
+        public event Action? DarkModeStateChanged;
+        public event Action? SideBarStateChanged;
 
-        public event Action DarkModeStateChanged;
-        public event Action SideBarStateChanged;
+
+        public void NavigateHome()
+        {
+            _navigationManager.NavigateTo("/", forceLoad: true);
+        }
+        public async Task ScrollFunctionality()
+        {
+            await _jsRuntime.InvokeVoidAsync("scrollFunctionality");
+        }
+
+        public async Task CloseAppBar()
+        {
+            await _jsRuntime.InvokeVoidAsync("closeAppBar");
+        }
 
         public void ChangeDarkModeState(bool? darkModeIsOn = null)
         {
